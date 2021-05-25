763b879090384c6e13c0f409a039695605584c29
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-05-25 17:24:03 -0500

    Imports into App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Header from './components/Header'
+import Post from './components/Post'
+import Media from './components/Media'
+import Content from './components/Content'
 
 function App() {
   return (
