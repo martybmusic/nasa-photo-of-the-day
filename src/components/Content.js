00f65c49aac27fdeb7d947ef8a0ed3e78569dc85
Martin Brueggemann <marty.brueggemann@Martins-MacBook-Pro.local>
2021-05-27 21:09:46 -0500

    Applied Styling to Content Component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import styled from 'styled-components'
 
+const StyledContent = styled.div`
+  width: 80%;
+  display: flex;
+  justify-content: space-evenly;
+  color: ${(props) => props.theme.primaryColor};
+  &:hover {
+    color: ${props => props.theme.secondaryColor};
+  }
+  button {
+     background-color: #007AAF;
+  }
+`
 const Content = (props) => {
     const { info, title, date } = props;
 
     return (
+        <StyledContent>
         <container>
             <h2>{title}</h2>
             <h3>{date}</h3>
             <p>{info}</p>
         </container>
+        </StyledContent>
     )
 }
 
